0364f5235473b87e505d2173da3b11a94147f7ce
David Josué Garcia <djgarciac@grupoautofacil.com>
2017-08-17 10:45:49 -0600

    Inicializar firebase

--- EventosFacilito/app/main.ts
+++ EventosFacilito/app/main.ts
@@ -3,6 +3,14 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+import firebase = require('nativescript-plugin-firebase');
+
+firebase.init({
+    persist: false
+})
+.then(instance => console.log("Firebase inicializado"))
+.catch(err => console.log(err));
+
 // A traditional NativeScript application starts by initializing global objects, setting up global CSS rules, creating, and navigating to the main page. 
 // Angular applications need to take care of their own initialization: modules, components, directives, routes, DI providers. 
 // A NativeScript Angular app needs to make both paradigms work together, so we provide a wrapper platform object, platformNativeScriptDynamic, 
